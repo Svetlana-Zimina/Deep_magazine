2fc6d60f5bb7135bed16b9ef99fa0b26eae7a0b1
Svetlana Zimina <Zima271985@yandex.ru>
2024-03-31 20:42:54 +0300

    register simple tag for categories

--- deep1/deep/urls.py
+++ deep1/deep/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -10,4 +11,10 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += [path('__debug__/', include('debug_toolbar.urls')),]
+    urlpatterns += [
+        path('__debug__/', include('debug_toolbar.urls')),
+    ]
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
--- deep1/goods/views.py
+++ deep1/goods/views.py
@@ -1,35 +1,15 @@
 from django.shortcuts import render
 
+from goods.models import Products
+
 
 def catalog(request):
+
+    goods = Products.objects.all()
+
     context = {
         'title': 'Бездна - каталог',
-        'goods': [
-            {'image': 'deps/images/goods/deep_43.jpg',
-             'name': '"Бездна" №43',
-             'description': 'Журнал "Бездна" №43, печатная версия',
-             'price': 500.00},
-            {'image': 'deps/images/goods/deep_43.jpg',
-             'name': '"Бездна" №43 pdf',
-             'description': 'Журнал "Бездна" №43, pdf формат',
-             'price': 250.00},
-            {'image': 'deps/images/goods/sticker_1.jpg',
-             'name': 'Наклейки 1',
-             'description': 'Наклейки "Спелеокотик" вриант 1',
-             'price': 100.00},
-            {'image': 'deps/images/goods/sticker_2.jpg',
-             'name': 'Наклейки 2',
-             'description': 'Наклейки "Спелеокотик" вриант 1',
-             'price': 100.00},
-            {'image': 'deps/images/goods/deep_46.jpg',
-             'name': '"Бездна" №46',
-             'description': 'Журнал "Бездна" №46, печатная версия',
-             'price': 500.00},
-            {'image': 'deps/images/goods/deep_46.jpg',
-             'name': '"Бездна" №46 pdf',
-             'description': 'Журнал "Бездна" №46, pdf формат',
-             'price': 250.00}
-        ]
+        'goods': goods
     }
     return render(request, 'goods/catalog.html', context)
 
