b32e18512339743b1bf9e9ebb65c15ad15534bc2
Svetlana Zimina <Zima271985@yandex.ru>
2024-04-15 11:18:09 +0300

    cart ajax request and json response

--- deep1/carts/urls.py
+++ deep1/carts/urls.py
@@ -6,14 +6,14 @@ from carts import views
 app_name = 'carts'
 
 urlpatterns = [
-    path('cart-add/<slug:product_slug>/', views.cart_add, name='cart-add'),
+    path('cart-add/', views.cart_add, name='cart-add'),
     path(
-        'cart-change/<slug:product_slug>/',
+        'cart-change/',
         views.cart_change,
         name='cart-change'
     ),
     path(
-        'cart-remove/<int:cart_id>/',
+        'cart-remove/',
         views.cart_remove,
         name='cart-remove'
     ),
